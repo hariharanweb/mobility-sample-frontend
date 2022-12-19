dc88427ad071346d858a2dc732582c245c9b345e
prochetab1999 <procheta.b@thoughtworks.com>
2022-12-19 13:12:43 +0530

    [Procheta | Japneet] Add. autocomplete location search

--- src/components/LocationSearch.js
+++ src/components/LocationSearch.js
@@ -1,12 +1,41 @@
 import { useState } from 'react'
 import FormControl from '@mui/material/FormControl'
 import TextField from '@mui/material/TextField'
+import { Autocomplete, LoadScript } from '@react-google-maps/api';
 
 const LocationSearch = ({type, initialLocation, onLocationChange}) => {
     const [location, setLocation] = useState(initialLocation)
+    console.log(process.env.REACT_APP_API_KEY);
+  const [autocomplete,setAutoComplete]=useState(null);
+  
+  const onLoad =(autocomplete)=> {
+    console.log('autocomplete: ', autocomplete)
+
+    setAutoComplete(autocomplete);
+  }
+
+  const onPlaceChanged= ()=> {
+    if (autocomplete !== null) {
+      let place=autocomplete.getPlace();
+      console.log(place) //result of the selected location from dropdown
+      console.log("lat",place.geometry['location'].lat());
+      console.log("lng",place.geometry['location'].lng());
+    }
+  }
     return (
+        
         <FormControl fullWidth sx={{ m: 1 }} variant="filled">
-            <TextField label={type} variant="standard" defaultValue={location.display}/>
+            <LoadScript
+        googleMapsApiKey={process.env.REACT_APP_API_KEY}
+        libraries={["places"]}
+      >
+        <Autocomplete
+            onLoad={onLoad}
+            onPlaceChanged={onPlaceChanged}
+          >
+            <TextField fullWidth sx={{ m: 1 }} style={{"marginLeft":0}} label={type} variant="standard" defaultValue={location.display}/>
+            </Autocomplete>
+            </LoadScript>
         </FormControl>
     )
 }
