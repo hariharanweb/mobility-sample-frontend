342909be21ea0a7a0b6faeb89e698bd70e615f0f
Japneet Bhatia <japneet.kaur@thoughtworks.com>
2022-12-23 16:46:09 +0530

    [Japneet | Procheta] Refactor. jest config file

--- src/screens/SearchScreen.spec.js
+++ src/screens/SearchScreen.spec.js
@@ -3,24 +3,20 @@ import { __esModule } from "@testing-library/jest-dom/dist/matchers";
 import { render, waitFor, screen } from "@testing-library/react";
 import SearchScreen from "./SearchScreen";
 import "@testing-library/jest-dom";
-import { BrowserRouter } from "react-router-dom";
-import { LoadScript } from "@react-google-maps/api";
 
 jest.mock("react-router-dom", () => {
   const originalModule = jest.requireActual("react-router-dom");
   return {
-    __esModule: true,
     ...originalModule,
     useNavigate: jest.fn(),
   };
 });
 
 describe("Basic functionality", () => {
-  __esModule: true,
-    it("should display header", async () => {
-      render(<SearchScreen />);
-      expect(screen.getByText("ONDC Sample App"));
-    });
+  it("should display header", async () => {
+    render(<SearchScreen />);
+    expect(screen.getByText("ONDC Sample App"));
+  });
 
   it("should show find ride button", async () => {
     render(<SearchScreen />);
