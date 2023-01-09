42d09eb0b6389dc011dc647afc50d8fe3d35e215
prochetab1999 <procheta.b@thoughtworks.com>
2023-01-09 13:42:36 +0530

    [Procheta | Japneet] Refactor. context for select api

--- src/utilities/ContextBuilder.js
+++ src/utilities/ContextBuilder.js
@@ -1,10 +1,8 @@
-import moment from 'moment';
 import { v4 as uuid } from 'uuid';
 
 const getContext = (action) => ({
   action,
   transaction_id: uuid(),
-  timestamp: moment().format(),
   bpp_id: process.env.REACT_APP_SELLER_APP_ID,
   bpp_uri: process.env.REACT_APP_SELLER_APP_URL,
 });
