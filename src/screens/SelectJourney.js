070861da87024014bbcdd7c4607dab37ad5991dd
Japneet Kaur Bhatia <japneetk1111@gmail.com>
2023-01-17 17:37:56 +0530

    [Japneet | Procheta] Refactor. quote heading

--- src/screens/SelectJourney.js
+++ src/screens/SelectJourney.js
@@ -1,11 +1,11 @@
 /* eslint camelcase: 0 */
 import * as React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { Grid, IconButton, Typography } from '@mui/material';
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import { Grid } from '@mui/material';
 import Quote from '../components/Quote';
 import Api from '../api/Api';
 import Loader from '../components/Loader';
+import Header from '../components/Header';
 
 const SelectJourney = () => {
   const location = useLocation();
@@ -34,13 +34,7 @@ const SelectJourney = () => {
   }, [getSelectResult, loadingJourney]);
   const displayQuote = () => (
     <Grid container>
-      <Typography variant="h4" gutterBottom paddingX={4} paddingY={1}>
-        <IconButton color="black" onClick={gotoHome}>
-          <ArrowBackIosNewIcon fontSize="0.9em" />
-        </IconButton>
-        {' '}
-        Booking Details
-      </Typography>
+
       <Grid item xs={12}>
         <Quote
           bookingInformation={bookingInformation}
@@ -49,6 +43,11 @@ const SelectJourney = () => {
       </Grid>
     </Grid>
   );
-  return loadingJourney ? <Loader /> : displayQuote();
+  return (
+    <>
+      <Header title="Booking Details" onBackClick={gotoHome} />
+      {loadingJourney ? <Loader /> : displayQuote()}
+    </>
+  );
 };
 export default SelectJourney;
