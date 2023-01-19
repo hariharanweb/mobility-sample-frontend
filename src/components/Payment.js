83cd3d7ece98611610554652a988e90d866d7a6a
prochetab1999 <procheta.b@thoughtworks.com>
2023-01-19 12:23:42 +0530

    [Procheta | Priya] Refactor. init payment page

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Radio from '@mui/material/Radio';
 import './Payment.css';
@@ -6,80 +7,69 @@ import { Button, FormControlLabel } from '@mui/material';
 
 const containerStyle = 'item-with-border';
 
-const CashPayment = () => (
+const CashPayment = ({ onPaymentSelect, selectedValue }) => (
 
-  <Grid container className={containerStyle} display="flex">
-
-    <Grid
-      item
-      xs={1}
-      alignItems="center"
-      justifyContent="center"
-      display="flex"
-      paddingLeft={2}
-    >
-      <FormControlLabel
-        value="Cash"
-        control={(
-          <Radio
-            value="Cash"
-            name="radio-buttons"
-            inputProps={{ 'aria-label': 'Cash' }}
-          />
+  <Grid container className={containerStyle} display="flex" paddingLeft={4}>
+    <FormControlLabel
+      value="Cash"
+      control={(
+        <Radio
+          checked={selectedValue === 'Cash'}
+          value="Cash"
+          onChange={onPaymentSelect}
+          name="radio-buttons"
+          inputProps={{ 'aria-label': 'Cash' }}
+        />
 )}
-        label="Cash"
-      />
-
-    </Grid>
+      label="Cash"
+    />
 
   </Grid>
 );
 
-const OnlinePayment = () => (
+const OnlinePayment = ({ onPaymentSelect, selectedValue }) => (
 
-  <Grid container className={containerStyle} display="flex">
-
-    <Grid
-      item
-      xs={1}
-      alignItems="center"
-      justifyContent="center"
-      display="flex"
-      paddingLeft={2}
-    >
-      <FormControlLabel
-        value="UPI"
-        control={(
-          <Radio
-            value="UPI"
-            name="radio-buttons"
-            inputProps={{ 'aria-label': 'UPI' }}
-          />
+  <Grid container className={containerStyle} display="flex" paddingLeft={4}>
+    <FormControlLabel
+      value="UPI"
+      control={(
+        <Radio
+          checked={selectedValue === 'UPI'}
+          value="UPI"
+          onChange={onPaymentSelect}
+          name="radio-buttons"
+          inputProps={{ 'aria-label': 'UPI' }}
+        />
 
 )}
-        label="UPI"
-      />
+      label="UPI"
+    />
 
+  </Grid>
+);
+
+const Payment = () => {
+  const [paymentMode, setPaymentMode] = useState('');
+  const onPaymentSelect = (event) => {
+    setPaymentMode(event.target.value);
+  };
+  return (
+
+    <Grid container paddingX={4}>
+      <>
+        <CashPayment onPaymentSelect={onPaymentSelect} selectedValue={paymentMode} />
+        <OnlinePayment onPaymentSelect={onPaymentSelect} selectedValue={paymentMode} />
+        <Button
+          fullWidth
+          variant="contained"
+          sx={{ my: 2 }}
+          disabled={!(paymentMode.length > 0)}
+        >
+          Book Now
+        </Button>
+      </>
     </Grid>
-
-  </Grid>
-);
-
-const Payment = () => (
-
-  <Grid container paddingX={4}>
-    <>
-      <CashPayment />
-      <OnlinePayment />
-      <Button
-        fullWidth
-        variant="contained"
-        sx={{ my: 2 }}
-      >
-        Book Now
-      </Button>
-    </>
-  </Grid>
-);
+  );
+};
 
 export default Payment;
