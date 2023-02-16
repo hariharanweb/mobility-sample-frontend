bfe4259525e8cb5f1e124af61dd4a832ed85d3bc
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-02-16 14:05:28 +0530

    [Japneet | Priya] Add. Map frame

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,7 +3,14 @@ import './Map.css';
 
 const Mapper = () => (
   <div className="map">
-    Map here
+    <iframe
+      title="myFrame"
+      src="http://localhost:52616/tracking"
+      width="100%"
+      height="100%"
+      frameBorder="0"
+      aria-hidden="false"
+    />
   </div>
 );
 
