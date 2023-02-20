3ca145afb404b374f997f0da7401d79f90073b6d
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-02-20 14:14:43 +0530

    [Japneet | Priya] Add. loading component

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
 import './Loader.css';
-// import CircularProgress from '@mui/material/CircularProgress';
 
 const Loader = () => (
-  <Grid className="loaderGrid" container paddingY="35%" paddingX="5%">
-    <h3>Finds perfect rides for you.</h3>
-    <div className="loader" />
+  <Grid container paddingY="25%" display="flex" flexDirection="column">
+    <h3 className="loader-heading">Finds perfect rides for you.</h3>
+    <div className="loader-image" />
   </Grid>
 );
 
