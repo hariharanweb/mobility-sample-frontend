b74099749404e541c992fcc3111f540e698688f2
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-02-22 16:18:09 +0530

    Modify. time in item

--- src/components/Time.js
+++ src/components/Time.js
@@ -6,11 +6,11 @@ import './Time.css';
 const formattedTime = (time) => moment(time).format('hh:mm');
 
 const Time = ({ time }) => (
-  <Typography className="time" variant="overline" gutterBottom>
-    {formattedTime(time.range.start)}
+    <Typography className="time" variant="overline" gutterBottom>
+      {formattedTime(time.range.start)}
     &nbsp;-&nbsp;
-    {formattedTime(time.range.end)}
-  </Typography>
+      {formattedTime(time.range.end)}
+    </Typography>
 );
 
 export default Time;
