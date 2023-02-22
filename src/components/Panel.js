320f59a8e547646fa6e865b73409b42b986c2e44
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-02-22 12:47:15 +0530

    Refactor. panel

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -76,7 +76,7 @@ function Panel(props) {
           }}
         >
           <Puller />
-          <Typography sx={{ p: 2, color: 'text.secondary' }}> Search </Typography>
+          <Typography sx={{ p: 2, color: 'text.secondary' }}>Search </Typography>
         </StyledBox>
         <StyledBox
           sx={{
