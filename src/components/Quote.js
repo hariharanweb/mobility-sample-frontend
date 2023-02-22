7b0704cc73e2fd486eda65af5fe7f3aac80ed977
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-02-22 12:57:34 +0530

    Refactor. quote and quote summery

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,15 +3,12 @@ import Typography from '@mui/material/Typography';
 import {
   Grid, Button,
 } from '@mui/material';
-import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
-import DirectionsCarOutlinedIcon from '@mui/icons-material/DirectionsCarOutlined';
-import CurrencyRupeeOutlinedIcon from '@mui/icons-material/CurrencyRupeeOutlined';
 import './Quote.css';
 import Provider from './Provider';
 import './Item.css';
 import InputField from './InputField';
 import PriceBreakdown from './PriceBreakdown';
-import Price from './Price';
+import QuoteSummary from './QuoteSummary';
 
 const Quote = ({ bookingInformation, provider, onInitJourney }) => (
   <Grid container paddingX={4}>
@@ -25,85 +22,6 @@ const Quote = ({ bookingInformation, provider, onInitJourney }) => (
       </Grid>
     </Grid>
     <QuoteProvider bookingInformation={bookingInformation} onInitJourney={onInitJourney} />
-  </Grid>
-);
-
-const QuoteSummary = ({ bookingInformation }) => (
-  <Grid container className="quote-container">
-    <Grid
-      item
-      xs={3}
-      alignItems="center"
-      justifyContent="left"
-      display="flex"
-      paddingLeft={2}
-    >
-      {bookingInformation[0]?.message?.order?.items[0]?.descriptor?.images && (
-      <img
-        height={52}
-        width={52}
-        src={bookingInformation[0]?.message?.order?.items[0]?.descriptor?.images[0]}
-        alt="vehicle-icon"
-      />
-      )}
-    </Grid>
-    <Grid
-      item
-      xs={3}
-      alignItems="center"
-      justifyContent="left"
-      display="flex"
-      paddingLeft="7%"
-      color="#000"
-    >
-      <div>
-        <Typography variant="body1" style={{ color: 'grey' }}>
-          <AccessTimeOutlinedIcon style={{ fontSize: 'small', marginRight: '7px', color: 'grey' }} />
-          Ride
-        </Typography>
-        <Typography variant="body1" style={{ fontSize: 'medium' }}>
-          27 mins
-        </Typography>
-      </div>
-    </Grid>
-    <Grid
-      item
-      xs={3.5}
-      alignItems="center"
-      justifyContent="left"
-      display="flex"
-      paddingLeft="7%"
-      color="#000"
-    >
-      <div>
-        <Typography variant="body1" style={{ color: 'grey' }}>
-          <DirectionsCarOutlinedIcon style={{ fontSize: 'small', marginRight: '7px', color: 'grey' }} />
-          Vehicle
-        </Typography>
-        <Typography variant="body1" style={{ fontSize: 'medium' }}>
-          {bookingInformation[0]?.message?.order?.items[0]?.descriptor?.name}
-        </Typography>
-      </div>
-    </Grid>
-    <Grid
-      item
-      xs={2.5}
-      alignItems="center"
-      justifyContent="left"
-      display="flex"
-      paddingLeft="5%"
-    >
-      <div>
-        <Typography variant="body1" style={{ color: 'grey' }}>
-          <CurrencyRupeeOutlinedIcon style={{ fontSize: 'small', marginRight: '7px' }} />
-          Fare
-        </Typography>
-        <Typography>
-          <Price price={bookingInformation[0]?.message?.order?.quote?.price} />
-        </Typography>
-      </div>
-    </Grid>
-
   </Grid>
 );
 
