2bf49ad3db2a15f0bb9706167f3e53f4267b48be
priyajames29 <42799848+priyajames29@users.noreply.github.com>
2023-02-24 23:10:40 +0530

    [Priya] Refactor. Catalog and search Result screen

--- src/screens/SearchResult.js
+++ src/screens/SearchResult.js
@@ -2,10 +2,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Grid from '@mui/material/Grid';
-import { Typography } from '@mui/material';
-import { grey } from '@mui/material/colors';
-import { Box } from '@mui/system';
-import { styled } from '@mui/material/styles';
 import Api from '../api/Api';
 import Catalog from '../components/Catalog';
 import ContextBuilder from '../utilities/ContextBuilder';
@@ -78,41 +74,10 @@ const SearchResult = () => {
     navigate('/', { state: {} });
   };
 
-  const drawerBleeding = 56;
-
-  const StyledBox = styled(Box)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'light' ? '#fff' : grey[800],
-  }));
-
-  const Puller = styled(Box)(({ theme }) => ({
-    width: 30,
-    height: 6,
-    backgroundColor: theme.palette.mode === 'light' ? grey[300] : grey[900],
-    borderRadius: 3,
-    position: 'absolute',
-    top: 8,
-    left: 'calc(50% - 15px)',
-  }));
-
   const displayCatalogs = () => (
     <Grid container>
-      <StyledBox
-        sx={{
-          position: 'absolute',
-          top: -drawerBleeding,
-          borderTopLeftRadius: 8,
-          borderTopRightRadius: 8,
-          right: 0,
-          left: 0,
-        }}
-      >
-        <Puller />
-        <Typography sx={{ color: 'text.secondary' }}>
-          <LocationTracer locationMap={locationMap} isSearchResult />
-        </Typography>
-      </StyledBox>
-
-      <Grid item xs={12} marginTop="12%">
+      <LocationTracer locationMap={locationMap} isSearchResult />
+      <Grid item xs={12}>
         {searchResults.map((bppProvider) => (
           <div>
             <Catalog
