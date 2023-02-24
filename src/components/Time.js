c532298d63dac55d68abe637857c12b0195d7d13
priyajames29 <42799848+priyajames29@users.noreply.github.com>
2023-02-24 22:58:51 +0530

    [Japneet] Add. search result screen train ui

--- src/components/Time.js
+++ src/components/Time.js
@@ -2,17 +2,16 @@ import React from 'react';
 import moment from 'moment';
 import Typography from '@mui/material/Typography';
 import './Time.css';
+
 const formattedTime = (time) => moment(time).format('hh:mm');
 const Time = ({ time }) => (
   <>
     {!!time.duration && (
-    <Typography variant="overline" gutterBottom>
     <Typography variant="subtitle2" gutterBottom>
         {time.duration}
     </Typography>
     )}
     {!!time.range && (
-    <Typography className="time" variant="overline" gutterBottom>
     <Typography className="time" variant="body1" gutterBottom>
       {formattedTime(time.range.start)}
     &nbsp;-&nbsp;
