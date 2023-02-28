571b3f224464da09e7d550be0665a04a6c38bffb
Nidhi Rajput <nidhirajput@MacBook-Pro.local>
2023-02-28 15:45:25 +0530

    [Nidhi | Japneet] Add.Toggle button for cabs and trains

--- src/components/FilterSection.js
+++ src/components/FilterSection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
 import { Grid } from '@mui/material';
@@ -7,23 +7,43 @@ import DirectionsSubwayIcon from '@mui/icons-material/DirectionsSubway';
 import './FilterSection.css';
 
 const style = {
-  borderRadius: '20px', marginLeft: '45px', borderColor: '#327B18',
+  borderRadius: '20px',
+  marginLeft: '45px',
+  borderColor: '#327B18',
 };
 
-const FilterSection = ({ openPanel }) => (
-  <Grid className="filter-icon" paddingTop={openPanel ? 2 : 3}>
-    <ButtonGroup>
-      <Button style={style}>
-        <DirectionsCarFilledIcon />
-        {' '}
-        Cabs
-      </Button>
-      <Button style={style}>
-        <DirectionsSubwayIcon />
-        Trains
-      </Button>
-    </ButtonGroup>
-  </Grid>
-);
+const FilterSection = ({ openPanel }) => {
+  const [enableCabs, setEnableCabs] = useState(false);
+  const [buttonClassCabs, setButtonClassCabs] = useState('button');
+
+  const [enableTrain, setEnableTrain] = useState(false);
+  const [buttonClassTrain, setButtonClassTrain] = useState('button');
+
+  const showCabs = () => {
+    setEnableCabs(!enableCabs);
+    setButtonClassCabs(enableCabs ? '' : 'clicked-button');
+  };
+
+  const showTrains = () => {
+    setEnableTrain(!enableTrain);
+    setButtonClassTrain(enableTrain ? '' : 'clicked-button');
+  };
+
+  return (
+    <Grid className="filter-icon" paddingTop={openPanel ? 2 : 3}>
+      <ButtonGroup>
+        <Button style={style} onClick={showCabs} className={buttonClassCabs}>
+          <DirectionsCarFilledIcon />
+          {' '}
+          Cabs
+        </Button>
+        <Button style={style} onClick={showTrains} className={buttonClassTrain}>
+          <DirectionsSubwayIcon />
+          Trains
+        </Button>
+      </ButtonGroup>
+    </Grid>
+  );
+};
 
 export default FilterSection;
