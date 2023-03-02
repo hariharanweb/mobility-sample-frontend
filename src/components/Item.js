b8ff62dfac30965136b7d5eb4b8fe5f6df6ebe51
priyajames29 <42799848+priyajames29@users.noreply.github.com>
2023-03-02 10:44:12 +0530

    [Priya] SearchResult Screen - Clickable card

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Typography from '@mui/material/Typography';
 // import Button from '@mui/material/Button';
 import TrainIcon from '@mui/icons-material/Train';
+import { CardActionArea } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
 import DirectionsCarOutlinedIcon from '@mui/icons-material/DirectionsCarOutlined';
 import CurrencyRupeeOutlinedIcon from '@mui/icons-material/CurrencyRupeeOutlined';
@@ -15,33 +18,37 @@ const Item = ({
   item, category, isParent, onSelectJourney, provider, fulfillments, bppUrl,
 }) => {
   const onSelect = () => {
-    onSelectJourney(item, provider, fulfillments, bppUrl);
+    const buttonEnabled = false;
+    onSelectJourney(item, provider, fulfillments, bppUrl, buttonEnabled);
   };
-  const containerStyle = isParent ? '' : 'item-with-border';
+  const containerStyle = isParent ? 'parent-item' : 'item-with-border';
   return (
-    <Grid container className={containerStyle} onClick={onSelect} display="flex" gap={2} sx={{ px: '10px', py: '23px', color: 'text.secondary' }}>
-      { item.descriptor.images && item.descriptor.images.length > 0
-        ? (
-          <Grid
-            item
-            xs={1}
-            alignItems="center"
-            justifyContent="center"
-            display="flex"
-            paddingLeft={2}
-            marginRight={1}
-          >
-            <img
-              height={32}
-              width={32}
-              src={item.descriptor.images[0]}
-              alt="taxi-icon"
-            />
-          </Grid>
-        ) : (
-          <TrainIcon className="train-icon" />
-        )}
-      {
+    <Card sx={{ maxWidth: 600 }} className="cardStyle">
+      <CardActionArea>
+        <CardContent onClick={onSelect}>
+          <Grid container className={containerStyle} display="flex" gap={2}>
+            { item.descriptor.images && item.descriptor.images.length > 0
+              ? (
+                <Grid
+                  item
+                  xs={1}
+                  alignItems="center"
+                  justifyContent="center"
+                  display="flex"
+                  paddingLeft={2}
+                  marginRight={1}
+                >
+                  <img
+                    height={32}
+                    width={32}
+                    src={item.descriptor.images[0]}
+                    alt="taxi-icon"
+                  />
+                </Grid>
+              ) : (
+                <TrainIcon className="train-icon" />
+              )}
+            {
         item.time
         && (
           <Grid
@@ -66,32 +73,32 @@ const Item = ({
         )
       }
 
-      <Grid
-        item
-        xs={3.5}
-        alignItems="center"
-        justifyContent="left"
-        display="flex"
-        paddingLeft="7%"
-        color="#000"
-      >
-        {isParent
+            <Grid
+              item
+              xs={3.5}
+              alignItems="center"
+              justifyContent="left"
+              display="flex"
+              paddingLeft="7%"
+              color="#000"
+            >
+              {isParent
         && (
         <Typography variant="body1" style={{ fontSize: 'small', fontWeight: '600' }}>
           {item.descriptor.name}
         </Typography>
         )}
-        {!isParent && category
+              {!isParent && category
         && (
         <Typography variant="body1">
           {category}
         </Typography>
         )}
-        {!isParent && !category
+              {!isParent && !category
         && (
-          <div>
-            <Typography variant="body1" style={{ color: 'grey' }}>
-              <DirectionsCarOutlinedIcon style={{ fontSize: 'small', marginRight: '7px', color: 'grey' }} />
+          <div className="vehicle">
+            <Typography variant="body1" style={{ color: 'grey', fontSize: 'small' }}>
+              <DirectionsCarOutlinedIcon className="icon" />
               Vehicle
             </Typography>
             <Typography variant="body1" style={{ fontSize: 'medium' }}>
@@ -99,42 +106,44 @@ const Item = ({
             </Typography>
           </div>
         )}
-      </Grid>
-      <Grid
-        item
-        xs={2.5}
-        alignItems="center"
-        justifyContent="left"
-        display="flex"
-        paddingLeft="5%"
-      >
-        <div>
-          <Typography variant="body1" style={{ color: 'grey' }}>
-            <CurrencyRupeeOutlinedIcon style={{ fontSize: 'small', marginRight: '3px', color: 'grey' }} />
-            Fare
-          </Typography>
-          <Typography variant="body1">
-            <Price price={item.price} />
-          </Typography>
-        </div>
+            </Grid>
+            <Grid
+              item
+              xs={!isParent && !category ? 1 : 4}
+              alignItems="center"
+              justifyContent="center"
+              display="flex"
+            >
+              <div>
+                <Typography variant="body1" style={{ color: 'grey', fontSize: 'small' }}>
+                  <CurrencyRupeeOutlinedIcon style={{ fontSize: 'small', marginRight: '3px', color: 'grey' }} />
+                  Fare
+                </Typography>
+                <Typography variant="body1">
+                  <Price price={item.price} />
+                </Typography>
+              </div>
 
-      </Grid>
-      {/* {!isParent && (
-      <Grid
-        item
-        alignItems="flex-end"
-        justifyContent="flex-end"
-        display="flex"
-        className="select-button"
-      >
-        <Typography variant="subtitle2" gutterBottom>
-          <Button onClick={onSelect} variant="contained">
-            Select
-          </Button>
-        </Typography>
-      </Grid>
-      )} */}
-    </Grid>
+            </Grid>
+            {!isParent && (
+              <Grid
+                item
+                alignItems="flex-end"
+                justifyContent="flex-end"
+                display="flex"
+                className="select-button"
+              >
+                {/* <Typography variant="subtitle2" gutterBottom>
+                  <Button onClick={onSelect} variant="contained">
+                    Select
+                  </Button>
+                </Typography> */}
+              </Grid>
+            )}
+          </Grid>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 };
 export default Item;
--- src/components/Price.js
+++ src/components/Price.js
@@ -5,16 +5,17 @@ const Price = ({ price }) => (
   <>
     {!!price.value && (
     <Typography variant="subtitle2" gutterBottom>
-      &nbsp;₹&nbsp;&nbsp;
+      ₹&nbsp;&nbsp;
       {price.value}
     </Typography>
     )}
     {!!price.minimum_value && !!price.maximum_value && (
     <Typography variant="subtitle2" gutterBottom>
-      &nbsp;₹
+      ₹
       {price.minimum_value}
-      &nbsp;-&nbsp;
-      &nbsp;₹
+      {' '}
+      -
+      ₹
       {price.maximum_value}
     </Typography>
     )}
