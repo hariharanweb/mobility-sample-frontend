5cf7a0f9f235af4cbd73764504137494e36a4eb5
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-03-03 16:16:31 +0530

    [Japneet] Add. route details stories

---
+++ src/components/RouteDetails.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RouteDetails = () => (
+  <div>RouteDetails</div>
+);
+
+export default RouteDetails;
---
+++ src/components/RouteDetails.stories.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import RouteDetails from './RouteDetails';
+
+export default {
+  title: 'RouteDetails',
+  component: RouteDetails,
+};
+// const routeDetails = [
+//   {
+//     routeName: 'Yellow Line',
+//     startLocation: {
+//       descriptor: {
+//         name: 'Huda City Center',
+//       },
+//       gps: '28.459435790417952, 77.0725014556516',
+//     },
+//     endLocation: {
+//       descriptor: {
+//         name: 'Samaypur Badli',
+//       },
+//       gps: '28.744842537205894, 77.1382827538096',
+//     },
+//     stops: [
+//       {
+//         descriptor: {
+//           name: 'Iffco Chowk',
+//         },
+//         gps: '28.744842537205894, 77.1382827538096',
+//       },
+//       {
+//         descriptor: {
+//           name: 'Huda City Center',
+//         },
+//         gps: '28.459435790417952, 77.0725014556516',
+//       },
+//       {
+//         descriptor: {
+//           name: 'ABCD',
+//         },
+//         gps: '28.744842537205894, 77.1382827538096',
+//       },
+//       {
+//         descriptor: {
+//           name: 'Samaypur Badli',
+//         },
+//         gps: '28.744842537205894, 77.1382827538096',
+//       },
+//       {
+//         descriptor: {
+//           name: 'M.G. Road',
+//         },
+//         gps: '28.744842537205884, 77.1382827538086',
+//       },
+//     ],
+//     frequency: [
+//       {
+//         times: [
+//           '2023-02-14T06:10:16+00:00',
+//           '2023-02-14T07:10:16+00:00',
+//           '2023-02-14T08:10:16+00:00',
+//           '2023-02-14T09:10:16+00:00',
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     routeName: 'Pink Line',
+//     startLocation: {
+//       descriptor: {
+//         name: 'Majlis Park',
+//       },
+//       gps: '28.459435790417952, 77.0725014556516',
+//     },
+//     endLocation: {
+//       descriptor: {
+//         name: 'Shiv Vihar',
+//       },
+//       gps: '28.744842537205894, 77.1382827538096',
+//     },
+//     stops: [
+//       {
+//         descriptor: {
+//           name: 'Punjabi Bagh West',
+//         },
+//         gps: '28.744842537205894, 77.1382827538096',
+//       },
+//       {
+//         descriptor: {
+//           name: 'Majlis Park',
+//         },
+//         gps: '28.459435790417952, 77.0725014556516',
+//       },
+//       {
+//         descriptor: {
+//           name: 'Shiv Vihar',
+//         },
+//         gps: '28.744842537205894, 77.1382827538096',
+//       },
+//       {
+//         descriptor: {
+//           name: 'Krishna Nagar',
+//         },
+//         gps: '28.744842537205884, 77.1382827538086',
+//       },
+//     ],
+//     frequency: [
+//       {
+//         times: [
+//           '2023-02-14T06:10:16+00:00',
+//           '2023-02-14T07:10:16+00:00',
+//           '2023-02-14T08:10:16+00:00',
+//           '2023-02-14T09:10:16+00:00',
+//         ],
+//       },
+//     ],
+//   },
+// ];
+
+export const Primary = () => (
+  <RouteDetails />
+);
