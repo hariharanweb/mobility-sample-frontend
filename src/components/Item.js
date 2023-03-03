d88509ff7f99692d2a2d996564607e4895d0be65
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-03-03 11:13:28 +0530

    Refactor. item

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,9 +2,9 @@ import Grid from '@mui/material/Grid';
 import React, { useState } from 'react';
 import Typography from '@mui/material/Typography';
 import TrainIcon from '@mui/icons-material/Train';
-import { CardActionArea } from '@mui/material';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+// import { CardActionArea } from '@mui/material';
+// import Card from '@mui/material/Card';
+// import CardContent from '@mui/material/CardContent';
 import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
 import DirectionsCarOutlinedIcon from '@mui/icons-material/DirectionsCarOutlined';
 import CurrencyRupeeOutlinedIcon from '@mui/icons-material/CurrencyRupeeOutlined';
@@ -30,33 +30,33 @@ const Item = ({
   const containerStyle = isParent ? 'parent-item' : 'item-with-border';
   const deselectedStateStyle = buttonEnabled ? 'white' : 'rgba(50, 123, 24, 0.1)';
   return (
-    <Card sx={{ maxWidth: 600 }} className="cardStyle" key={key}>
-      <CardActionArea>
-        <CardContent onClick={onSelect} style={{ padding: '0px' }} key={key}>
-          <Grid container className={containerStyle} display="flex" gap={10} sx={{ backgroundColor: keyState !== item?.id ? 'white' : deselectedStateStyle }}>
-            { item.descriptor.images && item.descriptor.images.length > 0
-              ? (
-                <Grid
-                  item
-                  xs={2}
-                  alignItems="center"
-                  display="flex"
-                  paddingLeft={2}
-                  marginRight={1}
-                  height="60px"
-                  width="90px"
-                >
-                  <img
-                    height={32}
-                    width={32}
-                    src={item.descriptor.images[0]}
-                    alt="taxi-icon"
-                  />
-                </Grid>
-              ) : (
-                <TrainIcon className="train-icon" />
-              )}
-            {
+  // <Card sx={{ maxWidth: 600 }} className="cardStyle" key={key}>
+  // {/* <CardActionArea> */}
+  // {/* <CardContent onClick={onSelect} style={{ padding: '0px' }} key={key}> */}
+    <Grid container className={containerStyle} display="flex" gap={10} style={{ padding: '0px' }} onClick={onSelect} key={key} sx={{ backgroundColor: keyState !== item?.id ? 'white' : deselectedStateStyle }}>
+      { item.descriptor.images && item.descriptor.images.length > 0
+        ? (
+          <Grid
+            item
+            xs={2}
+            alignItems="center"
+            display="flex"
+            paddingLeft={2}
+            marginRight={1}
+            height="60px"
+            width="90px"
+          >
+            <img
+              height={32}
+              width={32}
+              src={item.descriptor.images[0]}
+              alt="taxi-icon"
+            />
+          </Grid>
+        ) : (
+          <TrainIcon className="train-icon" />
+        )}
+      {
         item.time
         && (
           <Grid
@@ -84,20 +84,20 @@ const Item = ({
         )
       }
 
-            <Grid
-              item
-              xs={3.5}
-              alignItems="center"
-              justifyContent="center"
-              display="flex"
-              paddingLeft="4%"
-              paddingRight="3%"
-              paddingBottom="5px"
-              color="#000"
-              height="60px"
-              width="90px"
-            >
-              {isParent
+      <Grid
+        item
+        xs={3.5}
+        alignItems="center"
+        justifyContent="center"
+        display="flex"
+        paddingLeft="4%"
+        paddingRight="3%"
+        paddingBottom="5px"
+        color="#000"
+        height="60px"
+        width="90px"
+      >
+        {isParent
         && (
         <Typography
           variant="body1"
@@ -108,7 +108,7 @@ const Item = ({
           {item.descriptor.name}
         </Typography>
         )}
-              {!isParent && category
+        {!isParent && category
         && (
         <Typography
           variant="body1"
@@ -119,7 +119,7 @@ const Item = ({
           {category}
         </Typography>
         )}
-              {!isParent && !category
+        {!isParent && !category
         && (
           <div style={{
             alignContent: 'center', marginLeft: '15px', paddingTop: '6px',
@@ -137,32 +137,32 @@ const Item = ({
             </Typography>
           </div>
         )}
-            </Grid>
-            <Grid
-              item
-              xs={1.5}
-              alignItems="center"
-              display="flex"
-              paddingTop="8px"
-              paddingBottom="5px"
-              paddingLeft="8%"
-              height="70px"
-            >
-              <div>
-                <Typography style={{ color: 'grey', fontSize: 'small' }}>
-                  <CurrencyRupeeOutlinedIcon style={{ fontSize: 'small', color: 'grey' }} />
-                  Fare
-                </Typography>
-                <Typography variant="body1" style={{ fontSize: 'small', paddingLeft: '5%' }}>
-                  <Price price={item.price} />
-                </Typography>
-              </div>
+      </Grid>
+      <Grid
+        item
+        xs={1.5}
+        alignItems="center"
+        display="flex"
+        paddingTop="8px"
+        paddingBottom="5px"
+        paddingLeft="8%"
+        height="70px"
+      >
+        <div>
+          <Typography style={{ color: 'grey', fontSize: 'small' }}>
+            <CurrencyRupeeOutlinedIcon style={{ fontSize: 'small', color: 'grey' }} />
+            Fare
+          </Typography>
+          <Typography variant="body1" style={{ fontSize: 'small', paddingLeft: '5%' }}>
+            <Price price={item.price} />
+          </Typography>
+        </div>
 
-            </Grid>
-          </Grid>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+      </Grid>
+    </Grid>
+  // </CardContent>
+  // {/* </CardActionArea> */}
+  // </Card>
   );
 };
 export default Item;
