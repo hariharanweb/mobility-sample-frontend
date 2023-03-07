4bcb377b410915f6cda8ebbec15d7a38d3ce8ee2
HARIHARAN THIAGARAJAN <hariharanweb@gmail.com>
2023-03-07 10:59:48 +0530

    [Hari] Redo the panel some refactors

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -2,42 +2,29 @@ import React from 'react';
 import { Global } from '@emotion/react';
 import { styled } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { grey } from '@mui/material/colors';
 import Box from '@mui/material/Box';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import './Panel.css';
 
-const Root = styled('div')(({ theme }) => ({
-  height: '100%',
-  backgroundColor:
-    theme.palette.mode === 'light' ? grey[100] : theme.palette.background.default,
-}));
-
-const StyledBox = styled(Box)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'light' ? '#fff' : grey[800],
-}));
-
-const Puller = styled(Box)(({ theme }) => ({
-  width: '105px',
+const Puller = styled(Box)(() => ({
+  width: '25%',
   height: '4px',
-  backgroundColor: theme.palette.mode === 'light' ? grey[300] : grey[900],
-  borderRadius: '5px',
+  borderRadius: '1em',
   position: 'absolute',
-  top: '19px',
-  left: '127px',
+  top: '1em',
+  left: '36%',
 }));
 
-function Panel(props) {
-  const {
-    panelChildren,
-    open,
-    toggleDrawer,
-    openDrawerHeight, drawerHeight, panelHeight, isPullerPresent, isTransitionPresent,
-  } = props;
+const Panel = ({
+  panelChildren,
+  open,
+  toggleDrawer,
+  openDrawerHeight, drawerHeight, panelHeight, isPullerPresent, isTransitionPresent,
+}) => {
   const drawerBleeding = drawerHeight || 50;
   const height = panelHeight || '100%';
   return (
-    <Root>
+    <div className="panel-container">
       <CssBaseline />
       <Global
         styles={{
@@ -62,20 +49,21 @@ function Panel(props) {
         transitionDuration={isTransitionPresent ? { appear: 500, enter: 300, exit: 5 }
           : 5}
       >
-        <StyledBox
+        <Box
           className="panel-styledbox"
           paddingX={2}
           height={height}
           sx={{
             top: -drawerBleeding,
           }}
+          backgroundColor="#fff"
         >
           {isPullerPresent ? <Puller /> : null}
           {panelChildren}
-        </StyledBox>
+        </Box>
       </SwipeableDrawer>
-    </Root>
+    </div>
   );
-}
+};
 
 export default Panel;
